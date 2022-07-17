c9e5b3daf5757d1230b877ff9c1ee24645328efc
Catherine Cusick <catherine@cusick.cc>
2022-07-17 18:42:58 -0500

    Add one final test

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ function Phrase(content) {
 
   // Returns the letters in the content.
   this.letters = function letters() {
-    return (this.content.match(/[a-z]/gi) || []).join("");
+    const lettersRegEx = /[a-z]/gi;
+    return (this.content.match(lettersRegEx) || []).join("");
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
